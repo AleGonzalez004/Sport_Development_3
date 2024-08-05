143f59a7e4b58115727627d75095bbd58a2e130f
AleGonzalez004 <20220440@ricaldone.edu.sv>
2024-08-04 20:41:41 -0600

    Validaciones

--- controllers/public/recuperacion.js
+++ controllers/public/recuperacion.js
@@ -1,78 +1,67 @@
-// Constante para establecer el formulario de inicio de sesión.
-const LOGIN_FORM = document.getElementById('sessionForm');
-
-// Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
     // Se establece el título del contenido principal.
     MAIN_TITLE.textContent = 'Recupera contraseña';
-});
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('recoveryForm').addEventListener('submit', function (event) {
-        event.preventDefault(); 
-
-        let formData = new FormData(this);
-
-        fetch('../../api/helpers/recuperacion.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status) {
-                swal('Éxito', data.message, 'success');
-            } else {
-                swal('Error', data.message, 'error');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            swal('Error', 'Hubo un problema al procesar la solicitud.', 'error');
-        });
+    // Manejar el evento de mostrar el modal
+    const passwordModal = new bootstrap.Modal(document.getElementById('passwordModal'));
+    document.getElementById('passwordModal').addEventListener('show.bs.modal', function () {
+        // Limpiar campos del formulario de cambio de contraseña al mostrar el modal
+        document.getElementById('passwordChangeForm').reset();
     });
 
-    document.getElementById('passwordChangeForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        let formData = new FormData(this);
-
-        fetch('../../api/helpers/recuperacion.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.status) {
-                swal('Éxito', data.message, 'success').then(() => {
-                    window.location.href = '../../views/public/login.html'; 
-                });
-            } else {
-                swal('Error', data.message, 'error');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            swal('Error', 'Hubo un problema al procesar la solicitud.', 'error');
-        });
+    // Manejar el evento de ocultar el modal
+    document.getElementById('passwordModal').addEventListener('hide.bs.modal', function () {
+        // Limpiar campos del formulario de cambio de contraseña al ocultar el modal
+        document.getElementById('passwordChangeForm').reset();
     });
 });
 
-/*
-*   Función para obtener un token del reCAPTCHA y asignarlo al formulario.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-function reCAPTCHA() {
-    // Método para generar el token del reCAPTCHA.
-    grecaptcha.ready(() => {
-        // Constante para establecer la llave pública del reCAPTCHA.
-        const PUBLIC_KEY = '6LdBzLQUAAAAAJvH-aCUUJgliLOjLcmrHN06RFXT';
-        // Se obtiene un token para la página web mediante la llave pública.
-        grecaptcha.execute(PUBLIC_KEY, { action: 'homepage' }).then((token) => {
-            // Se asigna el valor del token al campo oculto del formulario
-            document.getElementById('gRecaptchaResponse').value = token;
-        });
+document.getElementById('recoveryForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    let formData = new FormData(this);
+
+    fetch('../../api/helpers/recuperacion.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status) {
+            swal('Éxito', data.message, 'success');
+        } else {
+            swal('Error', data.message, 'error');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        swal('Error', 'Hubo un problema al procesar la solicitud.', 'error');
     });
-}
+});
+
+document.getElementById('passwordChangeForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch('../../api/helpers/recuperacion.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status) {
+            swal('Éxito', data.message, 'success').then(() => {
+                window.location.href = '../../views/public/login.html'; 
+            });
+        } else {
+            swal('Error', data.message, 'error');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        swal('Error', 'Hubo un problema al procesar la solicitud.', 'error');
+    });
+});
